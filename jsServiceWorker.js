1d9152988106bd5a764e02414c0f4427f64fdbf4
fdsc <fdsc@yandex.ru>
2020-04-19 12:28:37 +0300

    Ещё ошибка

--- jsServiceWorker.js
+++ jsServiceWorker.js
@@ -36,56 +36,51 @@ self.addEventListener
 	'fetch',
 	function(event)
 	{
-		event.respondWith
+		// Загружаем из сети
+		var request = fetch(event.request)
+		.then
+		(
+			function (response)
+			{
+				let responseClone = response.clone();
+
+				// Кешируем заново
+				caches.open(version)
+				.then
+				(
+					function (cache)
+					{
+						cache.put(event.request, responseClone);
+					}
+				);
+
+				return response;
+			}
+		)
+		.catch
 		(
 			function()
 			{
-				// Загружаем из сети
-				return fetch(event.request)
+				// Возвращаем запрошенный ресурс из кеша
+				return caches.match(event.request)
 				.then
 				(
-					function (response)
+					function(response)
 					{
-						let responseClone = response.clone();
-
-						// Кешируем заново
-						caches.open(version)
-						.then
-						(
-							function (cache)
-							{
-								cache.put(event.request, responseClone);
-							}
-						);
-
-						return response;
-					}
-				)
-				.catch
-				(
-					function()
-					{
-						// Возвращаем запрошенный ресурс из кеша
-						return caches.match(event.request)
-						.then
-						(
-							function(response)
-							{
-								if (response !== undefined)
-								{
-									return response;
-								}
-								else
-								{
-									// return caches.match('error.png');
-									new Response('Network is unreilable or error occured');
-								}
-							}
-						);
+						if (response !== undefined)
+						{
+							return response;
+						}
+						else
+						{
+							// return caches.match('error.png');
+							new Response('Network is unreilable or error occured');
+						}
 					}
 				);
 			}
+		);
 
-		);	// event.respondWith end
+		event.respondWith(request);
 	}
 );
